d95307740a7f846ead42e85a1a3b33a7ab6ec458
papadonut9 <dolor3s@dolor3s>
2023-03-07 11:31:08 +0530

    debug: display keypresses

--- ascend.c
+++ ascend.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 struct termios orig_termios;
 
@@ -22,6 +24,13 @@ int main()
     enableRawMode();
 
     char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if (iscntrl(c)){
+            printf("%d\n", c);
+        }else{
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+    
     return 0;
 }
