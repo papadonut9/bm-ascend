d873f8bb5f9081f7cf0d6e493b5bec784def7526
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2023-03-08 12:58:22 +0530

    clearing screen onexit()

--- ascend.c
+++ ascend.c
@@ -15,6 +15,9 @@ struct termios orig_termios;
 /***  terminal  ***/
 void errhandl(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(69);
 }
@@ -72,6 +75,8 @@ void editorProcessKeypress()
     switch (c)
     {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(69);
         break;
 
