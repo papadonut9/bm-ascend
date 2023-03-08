3e0fd13e521a7c10ae5310843958144ef0e3f3e1
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2023-03-08 12:46:52 +0530

    keyboard input refactoring Also stopped printing out keypresses
    Co-authored-by: papadonut9 <47145773+papadonut9@users.noreply.github.com>
    Co-authored-by: papadonut9 <dolor3s@dolor3s>

--- ascend.c
+++ ascend.c
@@ -6,8 +6,8 @@
 #include <termios.h>
 #include <unistd.h>
 
-/***  defines  ***/ 
-#define CTRL_KEY(k) ((k) & 0x1f)
+/***  defines  ***/
+#define CTRL_KEY(k) ((k)&0x1f)
 
 /***  data  ***/
 struct termios orig_termios;
@@ -44,27 +44,42 @@ void enableRawMode()
         errhandl("tcsetattr");
 }
 
+char editorReadKey()
+{
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno != EAGAIN)
+            errhandl("read");
+    }
+    return c;
+}
+
+/***  input  ***/
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(69);
+        break;
+
+    default:
+        break;
+    }
+}
+
 /***  init utils  ***/
 int main()
 {
     enableRawMode();
 
     while (1)
-    {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) // EAGAIN won't work on CYGWIN systems
-            errhandl("read");
-        if (iscntrl(c))
-        {
-            printf("%d\r\n", c);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q'))
-            break;
-    }
+        editorProcessKeypress();
 
     return 0;
 }
