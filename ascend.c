0d7fae020726c64c43119df86345fecbbee541de
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2023-03-09 01:58:49 +0530

    fix: prevent moving cursor off screen

--- ascend.c
+++ ascend.c
@@ -232,17 +232,21 @@ void editorMoveCursor(int key){
     switch (key)
     {
     case ARROW_LEFT:
-      E.cx--;
-      break;
+        if(E.cx != 0)     
+            E.cx--;
+        break;
     case ARROW_RIGHT:
-      E.cx++;
-      break;
+        if(E.cx != E.screencols - 1)
+            E.cx++;
+        break;
     case ARROW_UP:
-      E.cy--;
-      break;
+        if(E.cy != 0)
+            E.cy--;
+        break;
     case ARROW_DOWN:
-      E.cy++;
-      break;
+        if(E.cy != E.screenrows - 1)
+            E.cy++;
+        break;
     }
 }
 
