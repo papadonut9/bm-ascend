56e31502fa49fd141a9dec7c5f68dd22ad058ab5
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2023-03-08 20:31:22 +0530

    feat: window sizing

--- ascend.c
+++ ascend.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -10,9 +11,12 @@
 #define CTRL_KEY(k) ((k)&0x1f)
 
 /***  data  ***/
-struct editorConfig{
+struct editorConfig
+{
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
-}E;
+} E;
 
 // struct editorconfig E;
 
@@ -64,11 +68,26 @@ char editorReadKey()
     return c;
 }
 
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+        return -1;
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /***  output  ***/
-void editorDrawRows(){
+void editorDrawRows()
+{
     // not this is a poor attempt by me to replicate vim's tildes to represent line numbers
-    int rows;
-    for(rows=0; rows < 24; rows++)
+    int lines;
+    for (lines = 0; lines < 24; lines++)
         write(STDOUT_FILENO, "~\r\n", 3);
 }
 
@@ -104,13 +123,22 @@ void editorProcessKeypress()
 }
 
 /***  init utils  ***/
+void editorInit()
+{
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        errhandl("getWindowSize");
+}
+
 int main()
 {
     enableRawMode();
+    editorInit();
 
     while (1)
+    {
         editorRefreshScreen();
-    editorProcessKeypress();
+        editorProcessKeypress();
+    }
     // disabled key output printing in debug. will enable that in next itr
 
     return 0;
