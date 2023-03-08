e2c8e887303ce8b6457355dbe98302d056de80b9
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2023-03-08 23:30:47 +0530

    hide cursor while re-rendering

--- ascend.c
+++ ascend.c
@@ -160,12 +160,14 @@ void editorRefreshScreen()
 {
     struct abuf ab = ABUF_INIT;
 
+    abAppend(&ab, "\x1b[?25l", 6);  // reset mode [http://vt100.net/docs/vt100-ug/chapter3.html#RM]
     abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);  // set mode [http://vt100.net/docs/vt100-ug/chapter3.html#SM]
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
