f3e3aa420073526de4994050f345b333b195e235
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2023-03-09 20:33:11 +0530

    erow definition

--- ascend.c
+++ ascend.c
@@ -28,11 +28,18 @@ enum editorKey
 
 /*** data ***/
 
+typedef struct erow{
+    int size;
+    char *chars;
+}erow;
+
 struct editorConfig
 {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow row;
     struct termios orig_termios;
 };
 
@@ -357,6 +364,7 @@ void editorInit()
 {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         errhandl("getWindowSize");
